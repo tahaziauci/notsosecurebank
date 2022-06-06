834d5032d1c1a815ff167851b7ffd2775b470a53
lon <lhl.nguyen@gmail.com>
2022-06-05 22:46:04 -0700

    Fix CWE-285 in withdraw function

--- src/main/java/com/example/Broken/Bank/service/UserServiceImpl.java
+++ src/main/java/com/example/Broken/Bank/service/UserServiceImpl.java
@@ -110,8 +110,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public ResponseEntity withdraw(HttpSession session, MoneyRequest moneyRequest) {
-        String userName = moneyRequest.getUsername(); // BADCODE (String) session.getAttribute(CURRENTUSER);
-        if (userName == null || !userRepository.existsUserByUsername(userName)) { // BADCODE: || !moneyRequest.getUsername().equals(userName)) {
+        String userName = (String) session.getAttribute(CURRENTUSER);
+        if (userName == null || !userRepository.existsUserByUsername(userName)
+                || !moneyRequest.getUsername().equals(userName)) {
             ErrorResponse msg = ErrorResponse
                     .builder()
                     .code(HttpStatus.UNAUTHORIZED.value())
